306dce1a6bfe589fb45944a74210c1b222a0e3d5
wmompie <wmompie@gmail.com>
2018-11-07 23:39:31 -0500

    Added some cards for testing

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
   return (
-    <div>
-      <img alt='robots' src='https://robohash.org/test?200x200' />
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
       <div>
-        <h2>Jane Doe</h2>
-        <p>jane.doe@gmail.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
