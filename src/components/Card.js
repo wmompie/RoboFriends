a4b6402d5a14991f4089592ac1dc6543a72572b0
wmompie <wmompie@gmail.com>
2018-11-15 00:23:38 -0500

    changed the url requirement for card images

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({name, email, id}) => {
   return (
     <div className="tc bg-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt='robots' src={`https://robohash.org/${id}?set=any`} />
+      <img alt='robots' src={`https://robohash.org/set_set3/${id}?size=200x200`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
